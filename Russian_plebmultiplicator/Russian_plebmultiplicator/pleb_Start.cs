5de34c70c731cb92b244c6bd641a4c1844895403
Philipp Brunzel <BrunzePh@BAIT.BBW.DD>
2018-05-03 11:54:25 +0200

    Fix funktionen

--- Russian_plebmultiplicator/Russian_plebmultiplicator/pleb_Start.cs
+++ Russian_plebmultiplicator/Russian_plebmultiplicator/pleb_Start.cs
@@ -8,5 +8,48 @@ namespace Russian_plebmultiplicator
 {
     public class pleb_Start
     {
+        internal static int[] Vergleichen(int x, int y)
+        {
+            int[] zahl1 = Zahl1_Dividieren(x);
+            int[] zahl2 = Zahl2_Multiplizieren(x, y);
+
+            for (int i = 0; i < zahl1.Length; i++)
+            {
+                if (zahl1[i] % 2 == 0)
+                {
+                    zahl2[i] = 0;
+                }
+            }
+            return zahl2;
+        }
+
+        internal static int[] Zahl1_Dividieren(int x)
+        {
+            List<int> zwischenergebnis = new List<int>();
+            zwischenergebnis.Add(x);
+            while (x != 1)
+            {
+                x = (x / 2);
+                zwischenergebnis.Add(x);
+            }
+            int[] ergebnis = zwischenergebnis.ToArray();
+            return ergebnis;
+        }
+
+        internal static int[] Zahl2_Multiplizieren(int x, int y)
+        {
+            int[] zahl1 = Zahl1_Dividieren(x);
+            List<int> zwischenergebnis = new List<int>();
+            for (int i = 0; i != zahl1.Length; i++)
+            {
+                zwischenergebnis.Add(y);
+                y = y * 2;
+            }
+            int[] ergebnis = zwischenergebnis.ToArray();
+            return ergebnis;
+        }
     }
+
+
+
 }
--- Russian_plebmultiplicator/Test_Pleb/pleb_Test.cs
+++ Russian_plebmultiplicator/Test_Pleb/pleb_Test.cs
@@ -11,10 +11,39 @@ namespace Test_Pleb
     [TestFixture]
     public class pleb_Test
     {
-        [Test, Category("Akzeptanztest")]
-        public void Programm_Test()
+        [Test, Category("Zahl1")]
+        public void Zahlx_länge()
         {
-            return;
+            var ergebnis = pleb_Start.Zahl1_Dividieren(47);
+            Assert.AreEqual(6, ergebnis.Length);
+        }
+
+        [Test, Category("Zahl1")]
+        public void Zahlx_array()
+        {
+            var ergebnis = pleb_Start.Zahl1_Dividieren(47);
+            Assert.AreEqual(1, ergebnis[5]);
+        }
+
+        [Test, Category("Zahl2")]
+        public void Zahly_länge()
+        {
+            var ergebnis = pleb_Start.Zahl2_Multiplizieren(47, 42);
+            Assert.AreEqual(6, ergebnis.Length);
+        }
+
+        [Test, Category("Zahl2")]
+        public void Zahly_array()
+        {
+            var ergebnis = pleb_Start.Zahl2_Multiplizieren(47, 42);
+            Assert.AreEqual(84, ergebnis[1]);
+        }
+
+        [Test, Category("Vergleichen")]
+        public void Vergleichen()
+        {
+            var ergebnis = pleb_Start.Vergleichen(47, 42);
+            Assert.AreEqual(0, ergebnis[4]);
         }
     }
 }
