d1bfb83b8d96b09b2dc3e3c5622e9df2cdb7db4e
Philipp Brunzel <BrunzePh@BAIT.BBW.DD>
2018-05-03 08:46:16 +0200

    Projekt Pleb template

---
+++ Russian_plebmultiplicator/Test_Pleb/pleb_Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Russian_plebmultiplicator;
+
+namespace Test_Pleb
+{
+    [TestFixture]
+    public class pleb_Test
+    {
+        [Test, Category("Akzeptanztest")]
+        public void Programm_Test()
+        {
+            return;
+        }
+    }
+}
